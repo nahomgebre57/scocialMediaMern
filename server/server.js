5b01f900a9e59ad05ff3c7fb57eba85b86998416
Nahom Gebre <nahomgebre73@gmail.com>
2025-05-27 13:40:59 +0300

    server update

--- server/server.js
+++ server/server.js
@@ -52,6 +52,11 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running. Welcome to the Social Media API!");
+});
+
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
